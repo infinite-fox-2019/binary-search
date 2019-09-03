3d1f8d30b6d3b1ed554ffdbc50704314b8f91db5
Andreas Holiwono <andreas_ho@hotmail.com>
2019-09-03 19:21:17 +0700

    Update binary.js

--- binary.js
+++ binary.js
@@ -4,16 +4,17 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  for(var i = 1; i < arr.length; i++) {
-    var temp = arr[i];
-    var index = i;
-    for(var j = i-1; j >= 0; j--) {
-        if(temp < arr[j]) {
-            arr[j+1] = arr[j];
-            index = j;
+  for(var i = 0; i < arr.length; i++) {
+    var temp = arr[i+1];
+    for(var j = i; j >= 0; j--) {
+      if(temp < arr[j]) {
+        arr[j+1] = arr[j];
+        if(temp >= arr[j-1] || arr[j-1] === undefined) {
+          arr[j] = temp;
+          break;
         }
+      }
     }
-    arr[index] = temp;
   }
   return arr;
 }
