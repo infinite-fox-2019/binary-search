ea8db9f281ad4c7273b18580a964ac6e717b9ef1
adityain98 <adityain98@gmail.com>
2019-09-03 15:24:52 +0700

    setengah benar

--- binary.js
+++ binary.js
@@ -3,21 +3,58 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
+// [ 8, 10, 10, 18, 22, 22, 32, 40, 90 ]
+// [ 3, 31, 89, 53, 53, 85, 77, 21, 55 ]
+
 function ownSort(arr) {
-  // Your sorting code
+  for(var i = 0; i<arr.length; i++){
+    if(i !== 0){
+      for(var j = i+1; j>=0; j--){
+        if(arr[j] <= arr[j-1]){
+          var swap = arr[j];
+          arr[j] = arr[j-1];
+          arr[j-1] = swap;
+        }
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var awal = 0;
+  var akhir = array.length-1;
+
+  for(var i = 0; i<1; i--){
+  var tengah = Math.floor((akhir + awal) / 2);
+    if(array[awal] == search || array[akhir] == search || array[tengah] == search){
+      if(array[awal] == search){
+        return awal;
+      }
+      else if(array[akhir] == search){
+        return akhir;
+      }
+      return tengah;
+    }
+    if(array[tengah] > search){
+      akhir = tengah - 1;
+    }
+    else{
+      awal = tengah + 1;
+    }
+
+    if(awal == akhir || awal == tengah || akhir == tengah){
+      return -1;
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(32, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
 
