df6c4eac3e3e8dbbb48e1912111618ec61443d78
devitasari <devitas700@gmail.com>
2019-09-03 19:11:32 +0700

    update

--- binary.js
+++ binary.js
@@ -5,28 +5,30 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  for (var i=0; i<arr.length; i++) {
-      for (var j=i-1; j>=0; j--) {
-          if (arr[j] > arr[j+1]) {
-              [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
-          }
-      }
-  }
-  return arr
+  for (var i = 1; i < arr.length; i++) {
+    var temp = arr[i];
+    var index = i;
+    for (var j = i - 1; j >= 0; j--) {
+        if (temp < arr[j]) {
+            arr[j + 1] = arr[j];
+            index = j;
+        } 
+    }
+    arr[index] = temp;
 }
-// console.log(ownSort([40, 18, 22, 32, 90, 10, 10, 22, 8]))
+return arr
+}
 function binary_search (search, array) {
   // Your searching code
     var indekstart = 0
     var indeksstop = array.length-1
     var mid = 1
     while (indekstart <= indeksstop) {
-        var mid = indekstart + Math.floor((indeksstop - indekstart)/2)
+        mid = indekstart + Math.floor((indeksstop - indekstart)/2)
         if (array[mid] == search) return mid
         if (search < array[mid]) {
             indeksstop = mid-1
-        }
-        else if (search > array[mid]) {
+        } else if (search > array[mid]) {
             indekstart = mid+1
         }
     }
