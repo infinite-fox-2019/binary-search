4c6c4c93a5182033d8356dcb41ff9ed0df867d59
afifahrahmak <afifahrk96@gmail.com>
2019-09-03 17:42:44 +0700

    insertionSort & binary search

--- binary.js
+++ binary.js
@@ -4,19 +4,45 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for (var i=1; i<arr.length; i++){
+    var store = arr[i];
+    for (var j=i-1; j>=0; j--){
+        if (store < arr[j]){
+            var swap = arr[j];        
+            arr[j] = arr[j+1];
+            arr[j+1] = swap;
+        }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+
+function binary_search (search, array) {
+  var min = 0
+  var max = array.length;
+  var med = Math.floor((max+min)/2)
+
+  var min = 0
+  var max = array.length
+  for(var i = 0; i < array.length; i++){
+    var med = Math.floor((min+max)/2)
+    if (search == array[med]){
+      return med
+    }else if (search<array[med]){
+      max = med 
+    }else {
+      min = med
+    }
+  }
+ return -1
 }
+
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-
-// Driver code
+console.log('ganjil ' + arrayGanjilSorted, 'genap ' + arrayGenapSorted)
+// // Driver code
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
